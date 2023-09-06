d0cc80e77b5dfe08d3d63743a864c93f932f3aff
sevenseasofbri <vishruti250102@gmail.com>
2023-09-06 13:28:40 +0800

    fixes to main

--- src/se/edu/inclass/Main.py
+++ src/se/edu/inclass/Main.py
@@ -4,44 +4,33 @@ from task.Task import Task
 from typing import List
 
 def main():
+    print("------------------------------------------------------------")
     print("Welcome to SimplyDoIt\n")
     dm = DataManager("./data/data.txt")
     tasks_data = dm.load_data()
 
     # Print data
-    print()
-    print("Deadlines before sorting")
+    print("------------------------------------------------------------")
+    print("Deadlines in the task list:")
     print_deadlines(tasks_data)
 
-    # Count deadlines
-    print("Total number of deadlines:", count_deadlines(tasks_data))
-
-    # Print deadlines using streams
-    print("\n")
-    print("Deadlines after sorting")
-    print_deadlines_using_stream(tasks_data)
-
     # Count deadlines using streams
+    print("\n")
     print("Total number of deadlines counted using streams:", count_deadlines_using_stream(tasks_data))
 
     # Filter tasks using streams
     filtered_list = filter_task_list_using_streams(tasks_data, "Client")
     print("\nFiltered list of tasks (filtered by keyword \"Client\"):")
     print_data(filtered_list)
+    print("------------------------------------------------------------")
 
-def count_deadlines(tasks_data: List[Task]) -> int:
-    count = 0
-    for task in tasks_data:
-        if isinstance(task, Deadline):
-            count += 1
-    return count
 
 def count_deadlines_using_stream(tasks: List[Task]) -> int:
     count = sum(1 for task in tasks if isinstance(task, Deadline))
     return count
 
 def print_data(tasks_data: List[Task]):
-    print("Printing data using iteration")
+    print("     Printing data using iteration")
     for task in tasks_data:
         print(task)
 
@@ -50,12 +39,6 @@ def print_deadlines(tasks_data: List[Task]):
     for task in tasks_data:
         if isinstance(task, Deadline):
             print(task)
-
-def print_deadlines_using_stream(tasks: List[Task]):
-    print("     Printing deadline using streams")
-    sorted_tasks = sorted(tasks, key=lambda x: x.get_description().lower())
-    for task in filter(lambda x: isinstance(x, Deadline), sorted_tasks):
-        print(task)
 
 def filter_task_list_using_streams(tasks: List[Task], filter_string: str) -> List[Task]:
     filtered_list = [task for task in tasks if filter_string in task.get_description()]
