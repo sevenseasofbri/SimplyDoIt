8afce3c6eb4c68150c06c858698ea9892e2670f6
sevenseasofbri <vishruti250102@gmail.com>
2023-09-06 19:26:27 +0800

    add print events in main.py

--- src/se/edu/inclass/Main.py
+++ src/se/edu/inclass/Main.py
@@ -1,6 +1,7 @@
 # Main.py is the entry point of the program
 from data.DataManager import DataManager
 from task.Deadline import Deadline
+from task.Event import Event
 from task.Task import Task
 from typing import List
 
@@ -20,6 +21,14 @@ def main():
     print("\n")
     print("Total number of deadlines counted using streams:", count_deadlines_using_stream(tasks_data))
 
+    print("\n")
+    print("Events in the task list:")
+    print_events(tasks_data)
+
+    # Count events using streams
+    print("\n")
+    print("Total number of events counted using streams:", count_events_using_stream(tasks_data))
+
     # Filter tasks using streams
     filtered_list = filter_task_list_using_streams(tasks_data, "Meeting")
     print("\nFiltered list of tasks (filtered by keyword \"Meeting\"):")
@@ -29,6 +38,11 @@ def main():
 # Counts total deadlines using streams
 def count_deadlines_using_stream(tasks: List[Task]) -> int:
     count = sum(1 for task in tasks if isinstance(task, Deadline))
+    return count
+
+# Counts total events using streams
+def count_events_using_stream(tasks: List[Task]) -> int:
+    count = sum(1 for task in tasks if isinstance(task, Event))
     return count
 
 # Prints data using iteration
@@ -42,6 +56,13 @@ def print_deadlines(tasks_data: List[Task]):
     print("     Printing deadline using iteration")
     for task in tasks_data:
         if isinstance(task, Deadline):
+            print(task)
+
+# Prints events using iteration
+def print_events(tasks_data: List[Task]):
+    print("     Printing events using iteration")
+    for task in tasks_data:
+        if isinstance(task, Event):
             print(task)
 
 # Filters tasks using streams
